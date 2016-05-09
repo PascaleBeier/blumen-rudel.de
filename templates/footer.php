ffed03c5b032ab5a912293254aa2334c0dbfbb4c
PascaleBeier <pascale@beier.io>
2016-05-10 00:02:15 +0200

    Add Site Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,20 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-md-3">
+        <h1>Hier finden Sie uns</h1>
+        Gärtnerei Werner Rudel<br>
+        Mühlenstraße 53a<br>
+        45701 Herten<br>
+        <a href="https://www.google.de/maps/place/G%C3%A4rtnerei+Werner+Rudel/@51.6109933,7.107006,17z/data=!3m1!4b1!4m13!1m7!3m6!1s0x47b8e51ebb027e69:0x7399da487bfb246f!2sM%C3%BChlenstra%C3%9Fe+53A,+45701+Herten!3b1!8m2!3d51.61167!4d7.10834!3m4!1s0x47b8e51ebdd7dff5:0x9da069ab2b1eafe8!8m2!3d51.61099!4d7.1092"
+        class="btn btn-default"><i class="fa fa-map-marker fa-fw"></i>Routenplaner</a>
+        <a href="tel:02366 18 39 74" class="btn btn-default"><i class="fa fa-phone fa-fw"></i>02366183974</a>
+
+      </div>
+      <div class="col-md-3 col-md-offset-6 footer-right">
+        <h1>Weitere Informationen</h1>
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+      </div>
+    </div>
   </div>
 </footer>
